6d3524b760f7cb94729a13f3d6ea38ccbeee13cd
გიორგი ენუქიძე <56786173+giorgi-333@users.noreply.github.com>
2026-01-31 16:28:14 +0400

    Add files via upload

---
+++ 3/main.js
@@ -0,0 +1,53 @@
+
+// document.write("ert");
+// alert(3)
+
+console.log(2)
+console.log("2")
+
+let x = 25;
+
+// const y = 3
+
+// let y = "giorgi"
+// let y = 3.44
+// let y = true / false
+
+// let y = null
+
+// let y // undifened
+
+let fristName;
+
+fristName = "giorgi"
+
+
+console.log(fristName)
+
+// let lastName
+// let last_name
+// let lastname
+// let last$name
+// let _lastName
+// let $lastName
+// let lastName123
+
+
+
+// a == b
+// a === b
+// >
+// <
+// <=
+// >=
+
+let a = 4
+let b = 7
+
+if(a > b) {
+    // 
+    console.log(a - b)
+} else {
+    console.log(b - a)
+}
+
