30b7b023b9f3d83370a6879f09d492e6f4f55cb7
giorgi-333 <giorgienuqidze2000@gmail.com>
2026-02-05 16:58:40 +0400

    Add exercise 5

---
+++ 5/main.js
@@ -0,0 +1,30 @@
+
+
+let i = 0;
+
+while (i < 10) {
+    i++
+    console.log(i + ")");
+    // i = i + 1
+    // i += 1
+    // i--
+}
+
+let y = 7
+
+do {
+    console.log(y);
+    y++;
+} while (y < 5)
+    
+
+for (let k = 5; k < 10; k++) {
+    console.log("K = ",k);
+}
+
+// let k = 5;
+
+// while (k < 10) {
+//     console.log("K = ", k);
+//     k++;
+// }
