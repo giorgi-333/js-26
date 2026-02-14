55bc4890fc505c602e0dc04ec9fb433462b61b59
გიორგი ენუქიძე <56786173+giorgi-333@users.noreply.github.com>
2026-02-14 14:09:56 +0400

    Add files via upload

---
+++ 9/main.js
@@ -0,0 +1,46 @@
+
+let qalaqebi = ["თბილისი","ქუთაისი","გორი","რუსთავი", "ფოთი"]
+
+
+// for(let i=0; i < qalaqebi.length; i++) {
+//     document.write(i, qalaqebi[i] + "<br>")
+// }
+
+
+let user = {
+    name: "giorgi",
+    age: 33,
+    location: "gori"
+}
+
+// document.write(user.name, user.age)
+
+let users = [
+    {
+        name: "giorgi",
+        age: 33,
+        location: "gori"
+    },
+    {
+        name: "nika",
+        age: 22,
+        location: "tbilisi"
+    },
+    {
+        name: "lika",
+        age: 33,
+        location: "gori"
+    }
+]
+
+for(let i=0; i< users.length; i++) {
+    // console.log(users[i])
+    document.write(`
+        <div class="item"> 
+            მომხმარებელი: 
+            ${users[i].name} <br>
+            ასაკი: ${users[i].age}
+        </div>
+        `)
+}
+
