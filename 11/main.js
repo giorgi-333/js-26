3bf4b0a2a8cc586f44c7946101cc097fb04bcc4a
giorgi-333 <giorgienuqidze2000@gmail.com>
2026-02-19 16:48:14 +0400

    Add 11 tut

---
+++ 11/main.js
@@ -0,0 +1,11 @@
+
+const textCont = document.getElementById("headerText");
+
+
+function jami() {
+    // document.body.innerHTML += `<h1>text</h1>`
+    textCont.className = "active";
+    textCont.style.backgroundColor = "orange";
+}
+
+// jami()
