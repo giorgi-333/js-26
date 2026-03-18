700f65728693b62518edb51d6ea176c349f29f95
giorgi-333 <giorgienuqidze2000@gmail.com>
2026-03-18 17:01:15 +0400

    Add tut 18

---
+++ 18/main.js
@@ -0,0 +1,17 @@
+
+
+let names = ["nika","giga","ana","beka"]
+let numbers = [5,7,23,45,22]
+
+names.sort()
+// names.reverse()
+
+// numbers.sort((a,b)=>{return a-b})
+
+numbers.sort((a,b)=>{return b-a})
+
+
+
+console.log(names)
+
+console.log(numbers);
